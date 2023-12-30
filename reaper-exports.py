de1fe5a9a682ad44e06d26b840513212ab4f116c
wormholecowboy <wormholecowboy@gmail.com>
2023-12-30 16:26:53 -0500

    reaper automator

--- reaper-exports.py
+++ reaper-exports.py
@@ -1,28 +1,34 @@
 from selenium import webdriver
-from selenium.webdriver.common import service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 import os
+import shutil
 
-s = Service("/opt/homebrew/bin/chromedriver")
+activate_script = "/Users/briangildea/scripts/venv-reaper-export/bin/activate"
+os.system(f"source {activate_script}")
 
-dir = "/Users/briangildea/scripts/"
+dir = "/Users/briangildea/reaper-files/reaper-auto-export/"
 files = os.listdir(dir)
-file_name = files[2]
+filtered = [file for file in files if file.endswith("mp3")]
+file_name = filtered[0]
 file_path = os.path.join(dir, file_name)
 abs_path = os.path.abspath(file_path)
-
+print(files)
+print(filtered)
+print(file_name)
+print(abs_path)
 
 def run():
     try:
-        print(file_path)
-        print(abs_path)
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
         driver = webdriver.Firefox()
         driver.get("http://192.168.86.20/")
         driver.implicitly_wait(10)
         driver.find_element(By.ID, "fileupload").send_keys(abs_path)
+        driver.implicitly_wait(10)
+        shutil.copy(abs_path, "/Users/briangildea/Music/Music/Media.localized/0-sketches/")
+        os.remove(abs_path) 
     except Exception as e:
         print(f"error: {e}")
 
 run()
-
--- reaper-exports.sh
+++ reaper-exports.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/bin/zsh
 #
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-for FILE in "$SCRIPT_DIR"/*; do
+# SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+DIR=/Users/briangildea/reaper-files/reaper-auto-export/
+export PATH=/Users/briangildea/.nvm/versions/node/v20.7.0/bin:/Users/briangildea/.nvm/versions/node/v20.7.0/bin:/Users/briangildea/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Users/briangildea/.nvm/versions/node/v20.7.0/bin:/Users/briangildea/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew/opt/fzf/bin:/opt/stylua:/usr/local/lf:/usr/local/go/bin:/opt/stylua:/usr/local/lf:/usr/local/go/bin
+ 
+for FILE in "$DIR"/*; do
     if [[ -f "$FILE" && "$FILE" == *.mp3 ]]; then
         BASENAME=$(basename "$FILE")
         FILENAME=${BASENAME%.mp3}
-        DIR=$(dirname "$FILE")
-        PARENT_DIR=$(basename "$DIR")
-        MONTH_YEAR=$(date +%Y+%m)
+        DIR_PATH=$(dirname "$FILE")
+        PARENT_DIR=$(basename "$DIR_PATH")
+        MONTH_YEAR=$(date +%Y-%m)
         YEAR=$(date +%Y)
         id3v2 -a "The Keeper" "${FILE}"
         id3v2 -A "$MONTH_YEAR" "${FILE}"
         id3v2 -t "$FILENAME" "${FILE}"
         id3v2 -g "sketches" "${FILE}"
-        id3v2 -y "$YEAR" ${FILE}"
+        id3v2 -y "$YEAR" "${FILE}"
+        # cp "${FILE}" ~/Music/Music/Media.localized/0-sketches/
     fi
 done
+
+python3 /Users/briangildea/scripts/reaper-exports.py
