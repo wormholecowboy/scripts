65a0c272fc817bd5e3b49aada4d65778762b5c83
wormholecowboy <wormholecowboy@gmail.com>
2024-07-22 17:58:34 -0400

    updated aws login

--- aws.sh
+++ aws.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 select_service() {
-  service_list=("glue" "lambda" "ec2")
+  service_list=("glue" "lambda" "s3")
   selected_service=$(printf "%s\n" "${service_list[@]}" | fzf --prompt "Select a service: " | xargs)
   case "$selected_service" in
     glue) select_glue_job
     ;;
     lambda) select_lambda
     ;;
-    s3) echo "s3"
+    s3) get_s3_bucket
     ;;
   esac
   
@@ -33,8 +33,8 @@ get_last_glue_log() {
     glue_url="/aws-glue/python-jobs/output"
     JOB_IDS=$(aws glue get-job-runs --job-name "$1" --query 'JobRuns[0].Id' --output text)
     JOB_ID=$(echo $JOB_IDS | tr ' ' '\n' | head -n 1)
-    echo $JOB_ID
-    aws logs get-log-events --log-group-name $glue_url --log-stream-name $JOB_ID
+    echo "$JOB_ID"
+    aws logs get-log-events --log-group-name "$glue_url" --log-stream-name "$JOB_ID"
     # log_stream=$(aws logs describe-log-streams --log-group-name "$glue_url" --order-by LastEventTime --descending --limit 1 --query "logStreams[0].logStreamName" --output text)
     # aws logs get-log-events --log-group-name "$glue_url" --log-stream-name "$log_stream" --query "events[*].[timestamp,message]" | jq
 }
@@ -57,24 +57,35 @@ get_last_lambda_log() {
     aws logs get-log-events --log-group-name "/aws/lambda/$1" --log-stream-name "$log_stream" --query "events[*].[timestamp,message]" | jq
 }
 
+get_s3_bucket() {
+  selected_bucket=$(aws s3 ls | awk '{print $3}' | fzf --prompt "Select a bucket: ")
+  # objects=$(aws s3 ls "s3://$selected_bucket/" --recursive)
+  # selected_object=$(echo "$objects" | fzf --prompt="Select a subfolder: ")
+  prefix=""
+  while true; do
+    folder=$(list_dirs "$selected_bucket" "$prefix")
+    echo "prefix: $prefix"
+    echo "folder: $folder"
+    sleep 7
+    if [[ -z "$folder" ]]; then
+      echo "loop ended"
+      break
+    fi
+    prefix="${prefix}${folder}"
+  done
+
+  final_url="s3://${selected_bucket}/${prefix}"
+  echo "This will be the final url: ${final_url}"
+
+  file_to_copy=$(ls | awk '{print #7}' | fzf) 
+}
+
+list_dirs() {
+  local bucket=$1
+  local prefix=$2
+
+  aws s3 ls "s3://${bucket}/${prefix}" | grep PRE | sed 's/.*PRE \(.*\)/\1/' | fzf --prompt="Select folder to build URL (ESC to finish): "
+}
+
 # Main script logic
 select_service
-
-
-
-
-
-
-
-
-
-# QUERY_ID=$(aws logs start-query \
-#  --profile $profile \
-#  --log-group-name /aws/lambda/test_client_getall_api_rds_lambda_function \
-#  --start-time `date --date='-60 minutes' "+Y-%m-%dT%H:%M:%S"` \
-#  --end-time `date "+%s"` \
-#  --query-string 'fields @message filter @message like /ERROR/' \
-#  | jq -r '.queryId')
-#  
-# echo "Query started (query id: $QUERY_ID), please hold ..." && sleep 5 # give it some time to query
-# aws logs --profile $profile logs get-query-results --query-id $QUERY_ID
--- export-aws-creds.sh
+++ export-aws-creds.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+saml2aws login
 output=$(saml2aws script | head -n 3)
 
 while IFS= read -r line; do
     eval "$line"
 done <<< "$output"
+
+echo "Successfully exported AWS variables"
