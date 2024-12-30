eba9089f6d7c1facecb569ba90805430787dfdd8
wormholecowboy <wormholecowboy@gmail.com>
2024-12-30 17:28:57 -0500

    Updated aws script

--- export-aws-creds.sh
+++ export-aws-creds.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 if ! saml2aws login; then
-  echo "Error: Check password/user and make sure you are connected to the VPN."
+  echo "\n"
+  echo "\n"
+  echo "ERROR: Check password/user and make sure you are connected to the VPN."
+  sleep 4
   exit 1
 fi
 
 output=$(saml2aws script | head -n 3)
 if [ $? -ne 0 ] || [ -z "$output" ]; then
+  echo "\n"
+  echo "\n"
   echo "Error: Failed to generate output from saml2aws script."
+  sleep 4
   exit 1
 fi
 
